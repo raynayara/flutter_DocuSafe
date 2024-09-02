b2fee622944888f6063a907d3ae53d7ff62537a0
raynayara <raysllannayara10@gmail.com>
2024-09-01 22:49:38 -0300

    first commit

---
+++ android/app/src/main/kotlin/Seguranca/informacao/flutter_docusafe/MainActivity.kt
@@ -0,0 +1,6 @@
+package Seguranca.informacao.flutter_docusafe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+    );
+  }
+}
