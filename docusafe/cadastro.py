4c9b92269b07c7301c47b1bd6e401af01608d941
Fernanda <fernandafelix789@gmail.com>
2024-09-04 14:58:31 -0300

    modiify

--- docusafe/cadastro.py
+++
@@ -1,9 +0,0 @@
-def cadastrar(user, password):
-    with open('users.txt', 'a') as file:
-        for line in file:
-            if f'{user} {password}' in line:
-                print('Usuário já cadastrado')
-                break
-            else:
-                file.write(f'{user} {password}\n')
-    
