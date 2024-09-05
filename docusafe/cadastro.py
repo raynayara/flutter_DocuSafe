52eab54efbe40849accc2680a75989733593a8c5
D-Ronald <danielronald21@gmail.com>
2024-09-04 21:15:07 -0300

    Login

--- docusafe/cadastro.py
+++ docusafe/cadastro.py
@@ -2,13 +2,17 @@ import json
 import os
 from hash import hash_password
 
-def cadastrar():
+# Nome do arquivo JSON
+nome_arquivo = 'users.json'
+    
+def coleta_dados():
     user = input('Digite o usuário: ')
     password = input('Digite a senha: ')
-    dado = {'user': user, 'password': hash_password(password)}
-
-    # Nome do arquivo JSON
-    nome_arquivo = 'users.json'
+    password_hashed = hash_password(password)
+    return user, password_hashed
+def cadastrar():
+    user, password_hashed = coleta_dados()
+    dado = {'user': user, 'password': password_hashed}
 
     # Verifica se o arquivo existe e carrega os dados
     if os.path.exists(nome_arquivo) and os.path.getsize(nome_arquivo) > 0:
@@ -36,3 +40,29 @@ def cadastrar():
         json.dump(dado_json, file, indent=4)
 
     print('Usuário cadastrado com sucesso')
+    
+
+def entrar():
+    user, password_hashed = coleta_dados()
+    
+    if os.path.exists(nome_arquivo) and os.path.getsize(nome_arquivo) > 0:
+        with open(nome_arquivo, 'r') as arquivo:
+            dado_json = json.load(arquivo)  # Carrega o arquivo JSON
+        
+        # Flag para controlar se o usuário foi encontrado
+        usuario_encontrado = False
+        
+        # Itera sobre a lista de usuários
+        for line in dado_json.get('users', []):
+            if line['user'] == user:
+                usuario_encontrado = True  # Usuário foi encontrado
+                if line['password'] == password_hashed:
+                    print('Usuário logado com sucesso')
+                    return  # Encerra a função se login for bem-sucedido
+                else:
+                    print('Senha inválida, tente novamente!')
+                    return  entrar()
+        
+        # Se o loop terminou e o usuário não foi encontrado
+        if not usuario_encontrado:
+            print('Usuário não cadastrado. Entre em contato com um dos ')
--- docusafe/main.py
+++ docusafe/main.py
@@ -1,8 +1,9 @@
 import cadastro 
 
 def app():
-    option = int(input('[1] Entrar\n[2] Cadastrar'))
-                
+    option = int(input('[1] Entrar\n[2] Cadastrar: '))
+    if option == 1:
+        cadastro.entrar()
     if option == 2:
         cadastro.cadastrar()
 
