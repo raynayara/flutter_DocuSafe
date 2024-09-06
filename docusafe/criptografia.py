ff1d82cbbc720f4f03d855b78aa35e05215ab941
Fernanda <fernandafelix789@gmail.com>
2024-09-06 10:55:42 -0300

    cripto

--- docusafe/criptografia.py
+++ docusafe/criptografia.py
@@ -36,55 +36,58 @@ class RSAKey:
         # Armazena as chaves no arquivo JSON
         with open(self.file_path, 'w') as file:
             json.dump(keys_data, file, indent=4)
-
-    def load_last_key(self):
-       
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r') as file:
-                keys_data = json.load(file)
-            if keys_data:
-                last_key = keys_data[-1]
-                return last_key['PRIVATE_KEYRSA'], last_key['PUBLIC_KEYRSA']
-        return None, None
     
 
     def cifrar(self, message):
-        _, public_key_str = self.load_last_key()
-        if public_key_str:
-            try:
-                public_key = RSA.import_key(public_key_str)
-                cipher = PKCS1_OAEP.new(public_key)
-                encrypted_message = cipher.encrypt(message.encode('utf-8'))
-                return encrypted_message
-            except Exception as e:
-                print(f"Ocorreu um erro ao criptografar a mensagem: {e}")
-                return None
-        return None
+     if os.path.exists(self.file_path):
+         with open(self.file_path, 'r') as file:
+             keys_data = json.load(file)
+         if keys_data:
+             _, public_key_str = keys_data[-1]['PRIVATE_KEYRSA'], keys_data[-1]['PUBLIC_KEYRSA']
+     else:
+         public_key_str = None
+ 
+     if public_key_str:
+         try:
+             public_key = RSA.import_key(public_key_str)
+             cipher = PKCS1_OAEP.new(public_key)
+             encrypted_message = cipher.encrypt(message.encode('utf-8'))
+             return encrypted_message
+         except Exception as e:
+             print(f"Ocorreu um erro ao criptografar a mensagem: {e}")
+             return None
+     return None
 
     def decifrar(self, encrypted_message):
-        private_key_str, _ = self.load_last_key()
-        if private_key_str:
-            try:
-                private_key = RSA.import_key(private_key_str)
-                cipher = PKCS1_OAEP.new(private_key)
-                decrypted_message = cipher.decrypt(encrypted_message).decode('utf-8')
-                return decrypted_message
-            except Exception as e:
-                print(f"Ocorreu um erro ao descriptografar a mensagem: {e}")
-                return None
-        return None
+     if os.path.exists(self.file_path):
+         with open(self.file_path, 'r') as file:
+             keys_data = json.load(file)
+         if keys_data:
+             private_key_str, _ = keys_data[-1]['PRIVATE_KEYRSA'], keys_data[-1]['PUBLIC_KEYRSA']
+     else:
+         private_key_str = None
+ 
+     if private_key_str:
+         try:
+             private_key = RSA.import_key(private_key_str)
+             cipher = PKCS1_OAEP.new(private_key)
+             decrypted_message = cipher.decrypt(encrypted_message).decode('utf-8')
+             return decrypted_message
+         except Exception as e:
+             print(f"Ocorreu um erro ao descriptografar a mensagem: {e}")
+             return None
+     return None
 
 if __name__ == "__main__":
     # Caminho do arquivo JSON onde as chaves serão armazenadas
     file_path = 'D:\\projetos\\flutter_DocuSafe\\docusafe\\keys.json'
     
-    # Cria uma instância do gerenciador de chaves e salva as chaves RSA
     rsa_manager = RSAKey(file_path)
     
-    # Exemplo de criptografia e descriptografia
+   
     # rsa_manager.save_rsa_keys_to_json()
 
-    #message = "acaba pelo amor de deus"
+    #message = "hello word"
     #encrypted_message = rsa_manager.cifrar(message)
     #
     #if encrypted_message:
